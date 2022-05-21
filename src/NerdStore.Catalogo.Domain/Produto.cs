13e62e04b15ba320a58ffbed0003e62e86b5e93d
Pablo Araújo <rafanet.live@gmail.com>
2022-05-21 01:24:46 -0300

    Criando entidade produyo

---
+++ src/NerdStore.Catalogo.Domain/Produto.cs
@@ -0,0 +1,40 @@
+﻿
+using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Catalogo.Domain
+{
+    public class Produto : Entity, IAggregateRoot
+    {
+        public Guid CategoriaId { get; set; }
+        public string Nome { get; private set; }
+        public string Descricao { get; private set; }
+        public bool Ativo { get; private set; }
+        public decimal Valor { get; private set; }
+        public DateTime DataCadastro { get; private set; }
+        public string Imagem { get; private set; }
+        public int QuantidadeEstoque { get; private set; }
+
+        public Categoria Categoria { get; private set; }
+
+        public Produto(string nome, string descricao, bool ativo, decimal valor, Guid categoriaId, DateTime dataCadastro, string imagem)
+        {
+            CategoriaId = categoriaId;
+            Nome = nome;
+            Descricao = descricao;
+            Ativo = ativo;
+            Valor = valor;
+            DataCadastro = dataCadastro;
+            Imagem = imagem;
+        }
+
+        public void Ativar() => Ativo = true;
+
+        public void Desativar() => Ativo = false;
+
+    }
+
+    public class Categoria : Entity
+    {
+
+    }
+}
