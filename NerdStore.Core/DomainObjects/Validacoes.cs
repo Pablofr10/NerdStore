6078fd59dc75da6a039d5b011da58bdab5e62720
Pablo Araújo <rafanet.live@gmail.com>
2022-05-21 22:36:41 -0300

    Adicionando validações no produto

--- NerdStore.Core/DomainObjects/Validacoes.cs
+++ NerdStore.Core/DomainObjects/Validacoes.cs
@@ -4,9 +4,13 @@ namespace NerdStore.Core.DomainObjects
 {
     public class Validacoes
     {
-        public static void ValidaSeIgual(object object1, object object2, string mensagem)
+        public static void ValidarSeIgual(object object1, object object2, string mensagem)
         {
             if (!object1.Equals(object2)) throw new DomainException(mensagem);
+        }
+        public static void ValidarSeDiferente(object object1, object object2, string mensagem)
+        {
+            if (object1.Equals(object2)) throw new DomainException(mensagem);
         }
         public static void ValidarCaracteres(string valor, int maximo, string mensagem)
         {
