669fdc4a33b4ce6af26d3601bb583a08f2fdd92e
Pablo Araújo <rafanet.live@gmail.com>
2022-05-22 00:31:26 -0300

    Criando contexto e mapeamentos.

--- src/NerdStore.Catalogo.Domain/Categoria.cs
+++ src/NerdStore.Catalogo.Domain/Categoria.cs
@@ -8,6 +8,10 @@ namespace NerdStore.Catalogo.Domain
         public string Nome { get; private set; }
         public int Codigo { get; private set; }
 
+        public ICollection<Produto> Produtos { get; set; }
+
+        protected Categoria(){}
+
         public Categoria(string nome, int codigo)
         {
             Nome = nome;
