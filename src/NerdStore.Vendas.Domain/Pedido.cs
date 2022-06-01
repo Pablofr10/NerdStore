e1e7f9c6d32dd2fbf04a7f9590d57ace3fd184b9
Pablo Araújo <rafanet.live@gmail.com>
2022-05-31 21:48:51 -0300

    Criando camadas de dados e aplicação

---
+++ src/NerdStore.Vendas.Domain/Pedido.cs
@@ -0,0 +1,33 @@
+﻿using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Vendas.Domain;
+
+public class Pedido : Entity, IAggregateRoot
+{
+    
+}
+
+public class PedidoItem : Entity
+{
+
+}
+
+public class Voucher : Entity
+{
+
+}
+
+public interface IPedidoRepository
+{
+
+}
+
+public enum PedidoStatus
+{
+
+}
+
+public enum TipoDescontoVoucher
+{
+
+}
